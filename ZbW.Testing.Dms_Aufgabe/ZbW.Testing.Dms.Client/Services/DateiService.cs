1a5334e6276033f07b9e9d70e00a7959f9e0b972
Daniel Etter <daniel_etter_06@hotmail.com>
2018-09-24 18:01:25 +0200

    Anpassungen

---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Services/DateiService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using ZbW.Testing.Dms.Client.Model;
+
+namespace ZbW.Testing.Dms.Client.Services
+{
+    class DateiService
+    {
+
+        public String getDateiNamenMetaFile(Guid docId)
+        {
+            string dateiname = String.Concat(docId, "Metadata.xml");
+            return dateiname;
+        }
+
+        public string getDateiName(Guid docId, string dateiTyp)
+        {
+            string dateiname = String.Concat(docId, "_Content", dateiTyp);
+            return dateiname;
+        }
+
+        public void oeffneDatei(MetadataItem mdI)
+        {
+            Process.Start(mdI._pfadNeu);
+        }
+
+        public String serealisieren(MetadataItem mdI)
+        {
+            XmlSerializer xmlseri = new XmlSerializer(typeof(MetadataItem));
+            StringWriter stringwrite = new StringWriter();
+            XmlWriter xmlwrite = XmlWriter.Create(stringwrite);
+            xmlseri.Serialize(xmlwrite, mdI);
+            var seriXml = stringwrite.ToString();
+            xmlwrite.Close();
+            return seriXml;
+
+        }
+
+        public MetadataItem deseralisiereMetaData(string path)
+        {
+            XmlSerializer xmlseri = new XmlSerializer(typeof(MetadataItem));
+            StreamReader lesen = new StreamReader(path);
+            var mdI = (MetadataItem) xmlseri.Deserialize(lesen);
+            lesen.Close();
+            return mdI;
+        }
+    }
+}
---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Services/RepoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZbW.Testing.Dms.Client.Services
+{
+    class RepoService
+    {
+        private String repoDir;
+
+        public string getRepo()
+        {
+            repoDir = ConfigurationManager.AppSettings.Get("RepositoryDir").ToString();
+            return repoDir;
+        }
+    }
+}
